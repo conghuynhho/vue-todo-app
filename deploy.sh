7c150ac8536f94405e6908afba20e26f689ac5ad
Huynh <conghuynhho12@gmail.com>
2021-07-18 10:14:04 +0700

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -6,4 +6,4 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:conghuynhho/todo-app.git master:gh-pages
+git push -f git@github.com:conghuynhho/vue-todo-app.git master:gh-pages
